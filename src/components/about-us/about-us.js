e7b63bd0843bf7f987d3e7d8a8cc4a14e4869fab
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2023-11-28 12:21:15 +0000

    Moved about-us to separate component and added sheetal's about

---
+++ src/components/about-us/about-us.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './about-us.css';
+
+export const AboutUs = () => {
+    return (
+        <div className='about-us'>
+            <h1>About us!</h1>
+
+            <h2>Lauren Cole</h2>
+            <p>My favourite hobby is playing video games. I like it because:
+                <ul>
+                    <li>It helps me relax</li>
+                    <li>I can do it alone</li>
+                </ul>
+            </p>
+
+            <br />
+
+            <h2>Sheetal Varsani</h2>
+            <p>I love crafting because:
+                <ul>
+                    <li>I enjoy learning new skills</li>
+                    <li>Being creative relaxes me and calms my mind</li>
+                </ul>
+            </p>
+        </div>
+    );
+};
