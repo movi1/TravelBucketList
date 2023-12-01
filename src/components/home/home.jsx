3f2bb13f4571fe0363c6846a68269624fb6e62ed
movi1 <98534830+movi1@users.noreply.github.com>
2023-12-01 14:57:59 +0000

    add hero image

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,24 @@
-// Home.js
+
+
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './home.css';
 
 export const Home = () => {
-    return <div className="main-content">Home Page</div>; 
-}
+    return (
+      
+            <div className="main-content animated-content">
+              <h1 className="title">Your Dream Destinations Await!</h1>
+              <p className="subtitle">Create Your Travel Bucket List and Start Exploring</p>
+              <p className="description">Discover amazing places, mark your favorites, and turn your travel dreams into reality.</p>
+              
+              {/* Example Link to the Bucket List Page */}
+              <Link to="/bucket-list">
+                <button className="explore-btn">Explore Bucket List</button>
+              </Link>
+            </div>
+    );
+};
+
 
 export default Home;
