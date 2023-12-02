705d5a46301f6b6c2ef7b8af95b7616fa5dc9c7c
movi1 <98534830+movi1@users.noreply.github.com>
2023-12-02 13:38:54 +0000

    update to homepage to add 3 sections to the site, needs further work

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,24 +1,46 @@
-
-
+// home.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './home.css';
 
 export const Home = () => {
-    return (
-      
-            <div className="main-content animated-content">
-              <h1 className="title">Your Dream Destinations Await!</h1>
-              <p className="subtitle">Create Your Travel Bucket List and Start Exploring</p>
-              <p className="description">Discover amazing places, mark your favorites, and turn your travel dreams into reality.</p>
-              
-              {/* Example Link to the Bucket List Page */}
-              <Link to="/bucket-list">
-                <button className="explore-btn">Explore Bucket List</button>
-              </Link>
-            </div>
-    );
+  return (
+    <div className="main-content animated-content">
+      {/* Container 1: Title, Subtitle, and Button */}
+      <div className="containers">
+      <div className="container">
+        <h1 className="title-home-page">Your Dream Destinations Await!</h1>
+        <p className="subtitle">Create Your Travel Bucket List and Start Exploring</p>
+        <p className="description">Discover amazing places, mark your favorites, and turn your travel dreams into reality.</p>
+        {/* Example Link to the Bucket List Page */}
+        <Link to="/bucket-list">
+          <button className="explore-btn">Explore Bucket List</button>
+        </Link>
+      </div>
+
+      {/* Container 2: Car Image */}
+      <div className="container">
+        <div className="image-container">
+          <img src="/images/car.png" alt="Car" />
+        </div>
+      </div>
+     
+        {/* Container 3: Meet the Team */}
+        <div className="container">
+          <h2>Meet the Team</h2>
+          <div className="team-members">
+            {/* Add your team member avatars with links to the about us page */}
+            <Link to="/about-us">
+              <img src="/images/movi-avatar.png" alt="Team Member 1" />
+            </Link>
+            {/* Repeat for other team members */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-
 export default Home;
+
+
