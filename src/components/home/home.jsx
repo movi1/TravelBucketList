a66acba3c464e8a674b1a3979d29ffda8d1bac27
movi1 <98534830+movi1@users.noreply.github.com>
2023-12-03 09:03:02 +0000

    add some animation to the row title need further work

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,32 +1,63 @@
 // home.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import "animate.css";
 import './home.css';
 
 export const Home = () => {
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimate(true); 
+    }, 500);
+  }, [])
+
   return (
-    <div className="main-content animated-content">
-      {/* Container 1: Title, Subtitle, and Button */}
-      <div className="containers">
-      <div className="container">
+    <div className="containers">
+      <div
+        className={`row-title animate__animated ${
+          animate ? "animate__fadeInDown" : ""
+        }`}
+      >
         <h1 className="title-home-page">Your Dream Destinations Await!</h1>
-        <p className="subtitle">Create Your Travel Bucket List and Start Exploring</p>
-        <p className="description">Discover amazing places, mark your favorites, and turn your travel dreams into reality.</p>
-        {/* Example Link to the Bucket List Page */}
-        <Link to="/bucket-list">
-          <button className="explore-btn">Explore Bucket List</button>
+
+        <p
+          className={`subtitle animate__animated ${
+            animate ? "animate__fadeInDown" : ""
+          } animate__delay-1s`}
+        >
+          Create Your Travel Bucket List and Start Exploring
+        </p>
+
+        <p
+          className={`description animate__animated ${
+            animate ? "animate__fadeInDown" : ""
+          } animate__delay-2s`}
+        >
+          Discover amazing places, mark your favorites, and turn your travel
+          dreams into reality.
+        </p>
+        <Link
+          to="/bucket-list"
+          className={`explore-btn animate__animated ${
+            animate ? "animate__fadeInDown" : ""
+          } animate__delay-3s`}
+        >
+          Explore Bucket List
         </Link>
       </div>
 
       {/* Container 2: Car Image */}
-      <div className="container">
+      <div className="row-image">
         <div className="image-container">
           <img src="/images/car.png" alt="Car" />
         </div>
       </div>
      
         {/* Container 3: Meet the Team */}
-        <div className="container">
+        <div className="row-team">
           <h2>Meet the Team</h2>
           <div className="team-members">
             {/* Add your team member avatars with links to the about us page */}
@@ -37,7 +68,7 @@ export const Home = () => {
           </div>
         </div>
       </div>
-    </div>
+    
   );
 };
 
