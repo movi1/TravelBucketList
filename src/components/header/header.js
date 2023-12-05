1ca1b059db240ce3f3a9878035a960949dcd0c0e
movi1 <98534830+movi1@users.noreply.github.com>
2023-12-05 22:54:18 +0000

    update the navbar and make responsive homepage and nav

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,31 +3,35 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import './header.css'; 
 
 export const Header = () => {
-  return (
-    <Navbar className="custom-navbar" expand="lg"  variant="dark">
-      <Container>
-        <Navbar.Brand href="/">
-          <img
-            src="./images/logo1.svg"
-            alt="logo"
-            style={{ height: '50px' }}
-          />
-          <h1 className="title">PACK & GO</h1>
-        </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/pack-list">Pack List</Nav.Link>
-            <Nav.Link href="/bucket-list">Bucket List</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+ 
+    return (
+      <Navbar className="custom-navbar" expand="lg" variant="dark">
+        <Container>
+        <Nav className="align-items-center">
+    <div className="navbar-brand-wrapper">
+      <div className="navbar-brand">
+        <img src="./images/logo1.svg" height="70" alt="Logo"/>
+        
+        <div>  
+          <h1 className="title">Pack & Go</h1> 
+        </div>
+      </div>
+    </div>
+  </Nav>
+    
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/pack-list">Pack List</Nav.Link>
+              <Nav.Link href="/bucket-list">Bucket List</Nav.Link>
+              <Nav.Link href="/signup-login">Sign Up / Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
 };
 
 export default Header;
