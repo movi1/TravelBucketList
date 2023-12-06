5a314181483feeffa1d98b6f699afe2913967605
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2023-12-06 18:27:41 +0000

    Moved testimonial-carousel to About Page

--- src/components/testimonial-carousel/TestimonialCarousel.test.jsx
+++ src/components/testimonial-carousel/TestimonialCarousel.test.jsx
@@ -9,19 +9,19 @@ describe("Carousel", () => {
     render(<TestimonialCarousel />);
   });
 
-  // Test that the 'testimonials-carousel' renders:
+  // Test that checks the 'testimonials-carousel' renders:
   it("should render the carousel component", () => {
     const carousel = screen.getByTestId("carousel");
     expect(carousel).toBeInTheDocument();
   });
 
-  // Test that the 'testimonials-tagline' renders:
+  // Test that checks the 'testimonials-tagline' renders:
   it("should render tagline", () => {
     const tagline = screen.getByText("See what our users have to say!");
     expect(tagline).toBeInTheDocument();
   });
 
-  // Test that the "Back to Top" element clicks and scrolls up to top:
+  // Test that checks the 'back-to-top' onClick element clicks and scrolls up to top:
   it("should scroll to the top when 'Back to Top' is clicked", () => {
     window.scrollTo = jest.fn();
     const backToTop = screen.getByText("Back to Top");
