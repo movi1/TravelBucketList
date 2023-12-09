687c4931a4f729b76cc16b536adf02370717185a
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2023-12-09 11:31:30 +0000

    Tidied up code and added comments to both jsx and css files for footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,17 +3,23 @@ import { Link } from "react-router-dom";
 
 import "./footer.css";
 
+// Define social media icons
 const facebook_icon = process.env.PUBLIC_URL + "/facebook_icon.png";
 const instagram_icon = process.env.PUBLIC_URL + "/instagram_icon.png";
 const email_icon = process.env.PUBLIC_URL + "/email_icon.png";
 
-export const Footer = () => {
-  const facebookLink = "https://www.facebook.com/";
-  const instagramLink = "https://www.instagram.com/";
+// Define social media links
+const facebookLink = "https://www.facebook.com/";
+const instagramLink = "https://www.instagram.com/";
 
+export const Footer = () => {
   return (
     <footer className="footer">
+
+      {/* Social Media Icons */}
       <div className="footer-icons">
+
+        {/* Facebook Icon */}
         <a href={facebookLink} target="_blank" rel="noopener noreferrer">
           <img
             className="social-icon"
@@ -21,6 +27,8 @@ export const Footer = () => {
             alt="Facebook Icon"
           />
         </a>
+
+        {/* Instagram Icon */}
         <a href={instagramLink} target="_blank" rel="noopener noreferrer">
           <img
             className="social-icon"
@@ -28,10 +36,14 @@ export const Footer = () => {
             alt="Instagram Icon"
           />
         </a>
+        
+        {/* Email Icon with link to Contact Us page */}
         <Link to="/contact-us">
           <img className="social-icon" src={email_icon} alt="Email Icon" />
         </Link>
       </div>
+
+      {/* Copyright information */}
       <div className="copyright">
         <p>Copyright © Pack & Go Inc 2023</p>
       </div>
