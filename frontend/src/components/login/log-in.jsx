1134247a501859539aa552960fde9f48f2109bf7
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2023-12-17 12:32:38 +0000

    added comments to log-in.jsx

--- frontend/src/components/login/log-in.jsx
+++ frontend/src/components/login/log-in.jsx
@@ -1,41 +1,48 @@
-import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import { loginApi } from './api';
-import WelcomeMessage from './welcome-message';
-import './login.css';
-import { useAuth } from './authContext';
-
-
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { loginApi } from "./api";
+import { useAuth } from "./authContext";
+import WelcomeMessage from "./welcome-message";
+import "./login.css";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  // State variables to manage form input, error, and login status
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Access the login function from the authentication context
   const { login } = useAuth();
+
+  // Access the navigation function from React Router
   const navigate = useNavigate();
 
+  // Function to handle the login attempt
   const handleLogin = async (e) => {
     e.preventDefault();
-  
+
     try {
+      // Call the login API with entered username and password
       const response = await loginApi(username, password);
-      console.log('Test');
-  
+      console.log("Test");
+
       if (response.success) {
         // Handle successful login
-        setIsLoggedIn(true)
+        setIsLoggedIn(true);
         login({ username });
-        console.log('Login successful');
+        console.log("Login successful");
       } else {
-        setError('Invalid username or password. Please try again.');
+        setError("Invalid username or password. Please try again.");
       }
     } catch (error) {
-      console.error('Login error:', error);
-      setError('Invalid username or password. Please try again.');
+      console.error("Login error:", error);
+      setError("Invalid username or password. Please try again.");
       e.preventDefault();
     }
   };
+
+  // Function to handle logout
   function handleLogout() {
     setIsLoggedIn(false);
   }
@@ -43,8 +50,10 @@ const Login = () => {
   return (
     <div className="login-container">
       {isLoggedIn ? (
+        // If logged in, display the welcome message component
         <WelcomeMessage username={username} onLogout={handleLogout} />
       ) : (
+        // If not logged in, display the login form
         <form onSubmit={handleLogin} className="login-form">
           <h2>Login</h2>
           <label htmlFor="login-username">Username:</label>
@@ -66,7 +75,9 @@ const Login = () => {
           <button type="submit" className="login-btn">
             Login
           </button>
+          {/* Display error message if there's an error */}
           {error && <p className="error-message">{error}</p>}
+          {/* Link to the signup page */}
           <p>
             Don't have an account? <Link to="/signup">Sign Up</Link>
           </p>
